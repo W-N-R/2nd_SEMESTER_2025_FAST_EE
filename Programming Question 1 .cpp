da2e2ead9df21ca1530a400de244f2569b18c858
W-N-R <waleednaeem133@gmail.com>
2025-01-26 23:18:45 +0500

    Equillilbirum index

--- Programming Question 1 .cpp
+++ Programming Question 1 .cpp
@@ -4,32 +4,32 @@
 //are equal to three times the index variable.Output the array so that
 //10 elements per line are printed.
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-void Main_Work(double parr[], int size) {
-	for (int i = 0; i < size; i++) {
-		if (i < 25) {
-			parr[i] = i * i;
-		}
-		else {
-			parr[i] = 3 * i;
-		}
-	}
-}
-
-	int main() {
-
-		double arr[50];
-		Main_Work(arr, 50);
-		for (int i = 0; i < 50; i++) {
-			if (i % 10 == 0) {
-				cout << endl;
-			}
-			cout << arr[i] << "\t";
-		}
-		return 0;
-
-	}
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//
+//void Main_Work(double parr[], int size) {
+//	for (int i = 0; i < size; i++) {
+//		if (i < 25) {
+//			parr[i] = i * i;
+//		}
+//		else {
+//			parr[i] = 3 * i;
+//		}
+//	}
+//}
+//
+//	int main() {
+//
+//		double arr[50];
+//		Main_Work(arr, 50);
+//		for (int i = 0; i < 50; i++) {
+//			if (i % 10 == 0) {
+//				cout << endl;
+//			}
+//			cout << arr[i] << "\t";
+//		}
+//		return 0;
+//
+//	}
 
