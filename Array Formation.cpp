d6a4238c331196a16c63c86d05ccb87d24360373
W-N-R <waleednaeem133@gmail.com>
2025-01-26 16:24:37 +0500

    get and print

--- Array Formation.cpp
+++ Array Formation.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
-
+void printArray(float parr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << parr[i] << " "<<endl;
+	}
+}
+void getArray(float parr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << "Enter the value of element " << i << " : ";
+		cin >> parr[i];
+	}
+}
 int main() {
 
+	float arr[5];
+	getArray(arr, 5);
 
+	cout << "so the array is :";
+	printArray(arr, 5);
 
 	return 0;
 }
