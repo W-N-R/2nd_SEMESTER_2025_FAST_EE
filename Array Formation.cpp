05450d758786b834c823b7f20a45139e690e8a39
W-N-R <waleednaeem133@gmail.com>
2025-01-26 16:32:50 +0500

    get, print, avg

--- Array Formation.cpp
+++ Array Formation.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 void printArray(float parr[], int size) {
 	for (int i = 0; i < size; i++) {
-		cout << parr[i] << " "<<endl;
+		cout << endl;
+		cout << parr[i] << " ";
 	}
 }
 void getArray(float parr[], int size) {
@@ -11,13 +12,21 @@ void getArray(float parr[], int size) {
 		cin >> parr[i];
 	}
 }
+void average(float parr[], int size) {
+	float sum = 0;
+	cout << endl;
+	for (int i = 0; i < size; i++) {
+		sum += parr[i];
+	}
+	cout << "The average of the array is : " << sum / size << endl;
+}
 int main() {
-
+	
 	float arr[5];
 	getArray(arr, 5);
 
 	cout << "so the array is :";
 	printArray(arr, 5);
-
+	average(arr, 5);
 	return 0;
 }
