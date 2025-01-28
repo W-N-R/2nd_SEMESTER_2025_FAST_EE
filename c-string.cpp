4121507ac106c0d74926bf567dc43a3ad99d7a90
W-N-R <waleednaeem133@gmail.com>
2025-01-28 17:36:21 +0500

    update

--- c-string.cpp
+++ c-string.cpp
@@ -4,10 +4,28 @@ using namespace std;
 
 int main() {
 
-	char name[7] = "WALEED";
+	char name[10] = "WALEED";
+	cout << endl;
 	cout << name;
-	char name2[10];
-	strcpy_s(name2, name);
+	char name2[10]="WNR";
+	cout << endl;
+	cout<<strcmp(name, name2);
+	cout << endl;
 	cout << name2;
+	cout << endl;
+	cout<<strlen(name);
+	cout << endl;
+	cout<<strlen(name2);
+	cout << endl;
+	char enter[50];
+	cin.get (enter,50);
+	cout << enter;
+	char discard;
+	cin.get(discard);
+	char enter2[50];
+	cout << endl;
+	cin.get(enter2, 50);
+	cout << enter2;
+
 	return 0;
 }
