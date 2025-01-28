3cb0861f767fa514e7e39528b575b58e8e65674c
W-N-R <waleednaeem133@gmail.com>
2025-01-28 07:45:20 +0500

    update

--- c-string.cpp
+++ c-string.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
 
 	char name[7] = "WALEED";
 	cout << name;
-
-
+	char name2[10];
+	strcpy_s(name2, name);
+	cout << name2;
 	return 0;
 }
