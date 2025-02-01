fd9876ab081a2fe8b0b0959ca0b21c8aa7ede595
W-N-R <waleednaeem133@gmail.com>
2025-02-02 00:20:11 +0500

    .

---
+++ binary_search_in_an_array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+//
+//void binary_search(int array[], int size_of_array, int search_element) {
+//	int start = 0;
+//	int end = size_of_array - 1;
+//	int mid = (start + end) / 2;
+//	while (start <= end) {
+//		if (array[mid] == search_element) {
+//			cout << "Element found at index " << mid;
+//			break;
+//		}
+//		else if (array[mid] < search_element) {
+//			start = mid + 1;
+//		}
+//		else {
+//			end = mid - 1;
+//		}
+//		mid = (start + end) / 2;
+//	}
+//	if (start > end) {
+//		cout << "Element not found";
+//	}
+//}
+
+void binary_search(int arr[], int size, int search) {
+	int start = 0;
+	int end = size;
+	while (start <= end) {
+
+	}
+
+}
+int main() {
+
+
+	return 0;
+}
