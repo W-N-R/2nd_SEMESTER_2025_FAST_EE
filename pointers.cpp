293136ab5634a8038f7d26e1aa84a57db7628e11
W-N-R <waleednaeem133@gmail.com>
2025-02-03 02:06:34 +0500

    .

--- pointers.cpp
+++ pointers.cpp
@@ -1,1 +1,28 @@
-.
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1 = 10,num2=9;
+	int* p;
+	int* p2;
+	p = &num1;
+	p2 = &num2;
+	cout << *p;
+	cout << endl;
+	cout << endl;
+	num2 = num1 - *p2;
+	cout << num2;
+	cout << endl;
+	int int1 = 26;
+	int int2 = 45;
+	int* int1Ptr = &int1;
+	int* int2Ptr = &int2;
+	*int1Ptr = 89;
+	*int2Ptr = 62;
+	int1Ptr = int2Ptr;
+	*int1Ptr = 80;
+	int1 = 57;
+	cout << int1 << " " << int2 << endl;
+	cout << *int1Ptr << " " << *int2Ptr << endl;
+	return 0;
+}
