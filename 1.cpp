1f8b6ce618fcaac0263b99e7d983fa25fa1bebbf
W-N-R <waleednaeem133@gmail.com>
2025-02-12 00:07:28 +0500

    .

--- 1.cpp
+++ 1.cpp
@@ -67,12 +67,31 @@ int main() {
 	*height = 2 * (*baseRadius);
 	baseRadius = new double;
 	*baseRadius = 4.0;
-	cout << "Radius of the base: " << baseRadius << endl;
-	cout << "Height: " << height << endl;
-	cout << "Volume: " << 3.14 * (baseRadius) * (baseRadius)
+	cout << "Radius of the base: " << *baseRadius << endl;
+	cout << "Height: " << *height << endl;
+	cout << "Volume: " << 3.14 * (*baseRadius) * (*baseRadius)
 		<< endl;
 	cout << "Surface area: "
-		<< 2 * 3.14 * (baseRadius) * (height) << endl;
+		<< 2 * 3.14 * (*baseRadius) * (*height) << endl;
+
+
+
+	int* myListPtr = new int[10];
+	int* yourListPtr;
+	*myListPtr = 100;
+	for (int i = 0; i < 10; i++)
+		myListPtr[i] = i * (i - 1);
+
+	yourListPtr = myListPtr;
+
+	for (int i = 0; i < 10; i++)  // Fix incorrect indexing
+		cout << myListPtr[i] << " ";
+
+	cout << endl;
+
+	delete[] yourListPtr;  // Move delete to the end to avoid dangling pointer
+
+
 	return 0;
 
 }
