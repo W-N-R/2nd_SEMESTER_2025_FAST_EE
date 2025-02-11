787b249c530a482a09a52c91fae7c646ef1b39f7
W-N-R <waleednaeem133@gmail.com>
2025-02-12 01:58:21 +0500

    .

--- 1.cpp
+++ 1.cpp
@@ -114,7 +114,19 @@ int main() {
 			delete[] _2DArray[i];
 		delete[] _2DArray;
 		
-
+		int* intList;
+		intList = new int[5];
+		for (int i = 0; i < 5; i++)
+			cin >> intList[i];
+		for (int i = 4; i > 0; i--)
+			intList[i] = intList[i] + intList[i - 1];
+		int sum = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			cout << intList[i] << " ";
+			sum = sum + intList[i];
+		}
+		cout << endl << "Sum = " << sum << endl;
 
 		return 0;
 	
