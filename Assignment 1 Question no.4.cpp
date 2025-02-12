2ad9d518cb29bc437e6e32503234b5b0bd75c350
W-N-R <waleednaeem133@gmail.com>
2025-02-12 18:43:53 +0500

    working

---
+++ Assignment 1 Question no.4.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Data {
+
+	    string name;
+		int rollnumber;
+		int batch;
+		string discipline;
+		float CGPA;
+
+};
+int main() {
+
+	ifstream data();
+	.yourRollnumber.txt
+	Data a, b, c, d;
+	cout << "please enter your name: ";
+	cin >> a.name;
+	cout << "please enter your Roll number: ";
+	cin >> a.rollnumber;
+	cout << "please enter your Batch: ";
+	cin >> a.discipline;
+	cout << "please enter your CGPA: ";
+	cin >> a.CGPA;
+
+	cout << endl;
+
+	cout << "please enter your name: ";
+	cin >> b.name;
+	cout << "please enter your Roll number: ";
+	cin >> b.rollnumber;
+	cout << "please enter your Batch: ";
+	cin >> b.discipline;
+	cout << "please enter your CGPA: ";
+	cin >> b.CGPA;
+
+	cout << endl;
+
+	cout << "please enter your name: ";
+	cin >> c.name;
+	cout << "please enter your Roll number: ";
+	cin >> c.rollnumber;
+	cout << "please enter your Batch: ";
+	cin >> c.discipline;
+	cout << "please enter your CGPA: ";
+	cin >> c.CGPA;
+
+	cout << endl;
+
+	cout << "please enter your name: ";
+	cin >> d.name;
+	cout << "please enter your Roll number: ";
+	cin >> d.rollnumber;
+	cout << "please enter your Batch: ";
+	cin >> d.discipline;
+	cout << "please enter your CGPA: ";
+	cin >> d.CGPA;
+
+	cout << endl;
+
+	return 0;
+
+
+}
