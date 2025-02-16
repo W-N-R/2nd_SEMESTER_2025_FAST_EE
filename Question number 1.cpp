72b6453c39f3266f36115a34252a5127f260815e
W-N-R <waleednaeem133@gmail.com>
2025-02-17 00:13:34 +0500

    .

--- Question number 1.cpp
+++ Question number 1.cpp
@@ -4,8 +4,26 @@
 
 using namespace std;
 
-void Display(int** x, int size) {
+void Display_positive(int* x, int size) {
+	int count = 0;
+	for (int i = 0; i < size; i++) {
+		if (x[i] > 0) {
+			count++;
+			cout << x[i] << endl;
+		}
+	}
+	cout << "The number of positive numbers in the array is: " << count << endl;
+}
 
+void Display_Negative(int* x, int size) {
+	int count = 0;
+	for (int i = 0; i < size; i++) {
+		if (x[i] < 0) {
+			count++;
+			cout << x[i] << endl;
+		}
+	}
+	cout << "The number of negative numbers in the array is: " << count << endl;
 }
 
 int main()
@@ -34,8 +52,11 @@ int main()
 		cout << ptr[i]<<endl;
 
 	}
-
+	cout << "Sorted Array" << endl;
+	Display_positive(ptr, size);
+	cout << "Negative numbers" << endl;
 	
+	Display_Negative(ptr, size);
 	delete[] ptr;
 	return 0;
 
