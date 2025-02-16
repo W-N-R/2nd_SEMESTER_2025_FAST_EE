47ed02ec8da67dd71b8f6bd4f60421eee5ed4e31
W-N-R <waleednaeem133@gmail.com>
2025-02-16 23:57:23 +0500

    .

--- Question number 1.cpp
+++ Question number 1.cpp
@@ -4,19 +4,34 @@
 
 using namespace std;
 
+void Display(int** x, int size) {
+
+}
 
 int main()
 {
 	
 	int size;
 	cin >> size;
-	int* ptr = new int(size);
+	int* ptr = new int [size];
+
+	srand(time(0));
 	
 	for (int i = 0; i < size;i++) {
-		 ptr[i] = rand();
+
+		int random_num = rand() % 201 - 100;
+
+		do {
+
+			ptr[i] = random_num;
+
+		} while (*ptr == 0);
 	}
+
 	for (int i = 0; i < size;i++) {
-		cout << ptr[i];
+
+		cout << ptr[i]<<endl;
+
 	}
 
 	
