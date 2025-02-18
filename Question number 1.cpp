b6ad28f5873cd880a3ae2d2295ae834209dd14d2
W-N-R <waleednaeem133@gmail.com>
2025-02-19 04:20:57 +0500

    .

--- Question number 1.cpp
+++ Question number 1.cpp
@@ -1,155 +1,85 @@
-//#include <iostream>
-//#include <cstdlib>
-//#include <ctime>
-//
-//using namespace std;
-//
-//void Display_positive(int* x, int size, int count) {
-//	
-//	for (int i = 0; i < size; i++) {
-//		if (x[i] > 0) {
-//			count++;
-//			cout << x[i] << endl;
-//		}
-//	}
-//	cout << "The number of positive numbers in the array is: " << count << endl;
-//}
-//
-//void Display_Negative(int* x, int size, int count) {
-//	
-//	for (int i = 0; i < size; i++) {
-//		if (x[i] < 0) {
-//			count++;
-//			cout << x[i] << endl;
-//		}
-//	}
-//	cout << "The number of negative numbers in the array is: " << count << endl;
-//}
-//
-//int main()
-//{
-//	
-//	int size;
-//	cout << "Please enter the number of elements in the main array or size of array: ";
-//	cin >> size;
-//	int* ptr = new int [size];
-//
-//	srand(time(0));
-//	
-//	for (int i = 0; i < size;i++) {
-//
-//		int random_num = rand() % 201 - 100;
-//
-//
-//		do {
-//
-//			ptr[i] = random_num;
-//
-//		} while (*ptr == 0);
-//	}
-//	cout << endl;
-//	cout << "so these are the random numbers" << endl;
-//	for (int i = 0; i < size;i++) {
-//
-//		cout << ptr[i]<<endl;
-//
-//	}
-//
-//
-//	cout << endl;
-//	cout << "Sorted Array of positive numbers: " << endl;
-//	int countpos = 0;
-//	Display_positive(ptr, size, countpos);
-//	cout  << endl;
-//	cout << "Negative numbers" << endl;
-//	int countneg = 0;
-//	Display_Negative(ptr, size	,countneg );
-//
-//	int * array_positives = new int [countpos];
-//	for (int i = 0; i < countpos; i++) {
-//		if (ptr[i] > 0) {
-//			array_positives[i] = ptr[i];
-//		}
-//	}
-//	delete[] ptr;
-//	return 0;
-//
-//
-//}
-//
-//
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-// Function to display array elements
+
 void Display(int** x, int size) {
     for (int i = 0; i < size; i++) {
-        cout << *x[i] << " ";
+        cout << *x[i] << "  ";
     }
     cout << endl;
 }
 
 int main() {
-    int s;
+
+    int size, total_pos_count = 0, total_neg_Count = 0;
+
     cout << "Enter the size of the array: ";
-    cin >> s;
+    cin >> size;
 
-    // Dynamic array allocation
-    int* arr = new int[s];
+  
+    int* main_array = new int[size];
 
-    // Seed for random number generation
-    srand(time(0));
+    	srand(time(0));
+ 	
+ 	for (int i = 0; i < size;i++) {
+ 
+ 		int random_num = rand() % 2001 - 1000;
+ 
+ 
+ 		do {
+ 
+ 			main_array[i] = random_num;
+ 
+ 		} while (*main_array == 0);
+ 	}
+ 	cout << endl;
+ 	cout << "So these are the random numbers" << endl;
+ 	for (int i = 0; i < size;i++) {
+ 
+ 		cout << main_array[i]<<endl;
+ 
+ 	}
+    cout << endl;
 
-    // Filling the array with random nonzero numbers
-    for (int i = 0; i < s; i++) {
-        arr[i] = (rand() % 200) - 100; // Generates numbers between -100 to 99
-        if (arr[i] == 0) {
-            arr[i] = 1; // Avoid zero by replacing it with 1
-        }
-    }
-
-    // Counting positive and negative numbers
-    int posCount = 0, negCount = 0;
-    for (int i = 0; i < s; i++) {
-        if (arr[i] > 0) {
-            posCount++;
+    for (int i = 0; i < size; i++) {
+        if (main_array[i] > 0) {
+            total_pos_count++;
         }
         else {
-            negCount++;
+            total_neg_Count++;
         }
     }
 
-    // Dynamic memory allocation for pointer arrays
-    int** posArr = new int* [posCount];
-    int** negArr = new int* [negCount];
+   
+    int** positive_Array = new int* [total_pos_count];
+    int** negative_Array = new int* [total_neg_Count];
 
-    // Storing pointers to positive and negative numbers
+    
     int posIndex = 0, negIndex = 0;
-    for (int i = 0; i < s; i++) {
-        if (arr[i] > 0) {
-            posArr[posIndex++] = &arr[i];
+    for (int i = 0; i < size; i++) {
+        if (main_array[i] > 0) {
+            positive_Array[posIndex++] = &main_array[i];
         }
         else {
-            negArr[negIndex++] = &arr[i];
+            negative_Array[negIndex++] = &main_array[i];
         }
     }
 
-    // Display positive numbers
-    cout << "Positive numbers: ";
-    Display(posArr, posCount);
+    
+    cout << "Array of Positive numbers: ";
+    Display(positive_Array, total_pos_count);
 
-    // Display negative numbers
-    cout << "Negative numbers: ";
-    Display(negArr, negCount);
+    
+    cout << "Array of Negative numbers: ";
+    Display(negative_Array, total_neg_Count);
 
-    // Free allocated memory
-    delete[] arr;
-    delete[] posArr;
-    delete[] negArr;
+    
+    delete[]  main_array;
+    delete[]  positive_Array;
+    delete[]  negative_Array;
 
     return 0;
 }
