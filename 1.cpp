f6fd633eead9aba05f73aee841760756d45cb735
W-N-R <waleednaeem133@gmail.com>
2025-02-20 11:18:59 +0500

    .

--- 1.cpp
+++ 1.cpp
@@ -143,3 +143,43 @@
 //	
 //
 //}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        array[i] = new int[cols];
+    }
+
+    cout << "Enter elements of the array:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> array[i][j];
+        }
+    }
+
+    cout << "The array is:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] array[i];
+    }
+    delete[] array;
+
+    return 0;
+}
