b278bdc41f9795277f1e715b774d911ede3f818d
W-N-R <waleednaeem133@gmail.com>
2025-03-22 16:27:41 +0500

    .

--- Array Formation.cpp
+++ Array Formation.cpp
@@ -1,32 +1,17 @@
-//#include<iostream>
-//using namespace std;
-//void printArray(float parr[], int size) {
-//	for (int i = 0; i < size; i++) {
-//		cout << endl;
-//		cout << parr[i] << " ";
-//	}
-//}
-//void getArray(float parr[], int size) {
-//	for (int i = 0; i < size; i++) {
-//		cout << "Enter the value of element " << i << " : ";
-//		cin >> parr[i];
-//	}
-//}
-//void average(float parr[], int size) {
-//	float sum = 0;
-//	cout << endl;
-//	for (int i = 0; i < size; i++) {
-//		sum += parr[i];
-//	}
-//	cout << "The average of the array is : " << sum / size << endl;
-//}
-//int main() {
-//	
-//	float arr[5];
-//	getArray(arr, 5);
-//
-//	cout << "so the array is :";
-//	printArray(arr, 5);
-//	average(arr, 5);
-//	return 0;
-//}
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	a obj("Waleed");
+
+	b obj2;
+	obj2.display();
+
+
+
+
+	return 0;
+
+}
