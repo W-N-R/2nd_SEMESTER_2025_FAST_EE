fd9deb5f96a2912c8d63f6574358e43eac2f2f03
W-N-R <waleednaeem133@gmail.com>
2025-04-20 15:28:34 +0500

    .

--- Student.h
+++ Student.h
@@ -1,47 +1,54 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-#include <iostream>
-#include <string>
-using namespace std;
-
-class a {
-protected:
-	string arr[100];
-public:
-	a();
-	a(string);
-};
-
-a::a() {
-	for (int i = 0; i < 100; i++) {
-		cin >> arr[i];
-	}
-}
-
-
-a::a(string data) 
-{
-	
-	for (int i = 0; i < 100; i++) {
-		arr[i] = data;
-	}
-}
-
-class b : public a {
-public:
-	string* ptr=arr;
-	void display();
-	b();
-
-};
-
-b::b() :a() {
-
-}
-
-void b::display() {
-	for (int i = 0; i < 100; i++) {
-		cout << arr[i] << endl;
-	}
-}
-#endif
+//#ifndef STUDENT_H
+//#define STUDENT_H
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class wnr_company {
+//public:
+//	int numberofemployees;
+//	string name[5];
+//public:
+//	wnr_company() {
+//		numberofemployees = 0;
+//		for (int i = 0; i <= 5;i++) {
+//			name[i] = " ";
+//		}
+//	}
+//	wnr_company(int n, string nm) {
+//		numberofemployees = n;
+//		for (int i = 0; i <= 5;i++) {
+//			name[i] = nm;
+//		}
+//	}
+//	void setnumberofemployees(int n) {
+//		numberofemployees = n;
+//	}
+//	void setname(string nm) {
+//		for (int i = 0; i <= 5;i++) {
+//			name[i] = nm;
+//		}
+//	}
+//	int getnumberofemployees() {
+//		return numberofemployees;
+//	}
+//	string getname() {
+//		for (int i = 0; i <= 5;i++) {
+//			return name[i];
+//		}
+//	}
+//	void print() {
+//		cout << "Company Name: " << name << endl;
+//		cout << "Number of Employees: " << numberofemployees << endl;
+//		for (int i = 0; i <= numberofemployees;i++) {
+//			cout << "Employee " << i << endl;
+//
+//			cout << "Name of the Employees :" << name[i] << endl;
+//		}
+//	}
+//	~wnr_company(){}
+//};
+//
+//
+//
+//#endif
