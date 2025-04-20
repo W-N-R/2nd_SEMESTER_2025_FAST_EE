fc80d94f8e7020d919771ce0fb14f11c91f8df0a
W-N-R <waleednaeem133@gmail.com>
2025-04-20 15:28:13 +0500

    .

---
+++ polymorphism.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include "Student.h"
+//using namespace std;
+//
+//int main() {
+//
+//		wnr_company emp;
+//		emp.setnumberofemployees(5);
+//		
+//		emp.setname("WNR");
+//		emp.print();
+//	
+//	return 0;
+//}
