634d2aee9bee4c513e3680b4606e73f515d40bd4
W-N-R <waleednaeem133@gmail.com>
2025-05-07 12:27:49 +0500

    .

--- lab final prep.cpp
+++ lab final prep.cpp
@@ -10,14 +10,60 @@
 //}
 
 
-#include <iostream>
-using namespace std;
-int main()
-{
-	int a = 32, * ptr = &a;
-	char ch = 129, & cho = ch;
-	cho += a;
-	*ptr -= ch + 20;
-	cout << *((char*)ptr) << ", " << (int)ch << endl;
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a = 32, * ptr = &a;
+//	char ch = 129, & cho = ch;
+//	cho += a;
+//	*ptr -= ch + 20;
+//	cout << *((char*)ptr) << ", " << (int)ch << endl;
+//	return 0;
+//}
+
+//#include <iostream>;
+//using namespace std;
+//int main()
+//{
+//	int x = 4, * xx = &x, ** xxx = &xx;
+//	cout << sizeof(x) << "," << sizeof(xx) << "," << sizeof(xxx);
+//	return 0;
+//}
+
+//
+//
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//using namespace std;
+//
+//template <class datatype>
+//datatype Calculate_Sum_n_Average(datatype n1, datatype n2, datatype n3) {
+//	datatype Total_sum,Total_average;
+//	Total_sum = n1 + n2 + n3;
+//	cout << "Total sum of three given inputs are:" << Total_sum << endl;
+//	Total_average = (n1 + n2 + n3) / 3;
+//	cout << "Total average of three given inputs are:" << Total_average << endl;
+//	return Total_sum, Total_average;
+//}
+//int main() {
+//	int num1, num2, num3;
+//	float num11, num22, num33;
+//	double num111, num222, num333;
+//	cout << "---------Integer Test--------" << endl;
+//	cin >> num1 >> num2 >> num3;
+//	Calculate_Sum_n_Average(num1, num2, num3);
+//	cout << endl << endl;
+//	cout << "-----------Float Test----------" << endl;
+//	cin >> num11 >> num22 >> num33;
+//	Calculate_Sum_n_Average(num11, num22, num33);
+//	cout << endl << endl;
+//	cout << "-----------Double Test----------" << endl;
+//	cin >> num111 >> num222 >> num333;
+//	Calculate_Sum_n_Average(num111, num222, num333);
+//	cout << endl << endl;
+//	return 0;
+//}
+
+
